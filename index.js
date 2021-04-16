e02b342149e6ebc45eef0746909257c6c19a3eb9
Jint3x <65330793+Jint3x@users.noreply.github.com>
2021-04-16 17:29:38 +0300

    Added CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors())
 
 app.get("/", (req, res) => res.send("Hello world!"));
-
 app.get("/api/header", (req, res) => res.json("My backend sucks!"));
 
 app.listen(process.env.port || 3000);
